52601818a1fa428fb46566822751d4d94d790935
Chris Everhart <chris.everhart@intele-gents.com>
2026-04-09 21:49:11 -0400

    fix the vpc id for the target group and move the alb listener code block over from the module 3 lab

--- module4/main.tf
+++ module4/main.tf
@@ -290,5 +290,16 @@ resource "aws_lb_target_group" "alb-lb-tg" {
   target_type = "instance"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.project.id
+  vpc_id      = aws_vpc.module_04_vpc.id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-lb-tg.arn
+  }
 }
