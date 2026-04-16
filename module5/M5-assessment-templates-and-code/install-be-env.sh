a84b14082b152850745276412341620d7e5ca422
Chris Everhart <chris.everhart@intele-gents.com>
2026-04-16 10:15:36 -0400

    update with information from module 4 and set variables.

--- module5/M5-assessment-templates-and-code/install-be-env.sh
+++ module5/M5-assessment-templates-and-code/install-be-env.sh
@@ -12,19 +12,34 @@ python3 -m pip install pillow
 python3 -m pip install boto3
 python3 -m pip install mysql-connector-python
 
+sudo apt install -y awscli
+
 cd /home/ubuntu
 
-# Command to clone your private repo via SSH usign the Private key
-####################################################################
-# Note - change "hajek.git" to be your private repo name (hawk ID) #
-####################################################################
-sudo -u ubuntu git clone git@github.com:jhajek/coursera-cloud-computing.git
+# Pull the GitHub deploy key from Secrets Manager
+aws secretsmanager get-secret-value --secret-id github-deploy-key --region us-east-1 --query SecretString --output text > /home/ubuntu/.ssh/github-deploy-key
+chmod 600 /home/ubuntu/.ssh/github-deploy-key
+chown ubuntu:ubuntu /home/ubuntu/.ssh/github-deploy-key
 
-# Start the nodejs app where it is located via PM2
-# https://pm2.keymetrics.io/docs/usage/quick-start
-cd /home/ubuntu/coursera-cloud-computing/itmo-463-563/M5L8-fix
+# Add GitHub to known hosts
+sudo -u ubuntu ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts
 
-echo "Copying ./app.py to /usr/local/bin/..." 
+# Configure SSH to use the deploy key
+cat > /home/ubuntu/.ssh/config <<EOF
+Host github.com
+  IdentityFile /home/ubuntu/.ssh/github-deploy-key
+  StrictHostKeyChecking no
+EOF
+chown ubuntu:ubuntu /home/ubuntu/.ssh/config
+chmod 600 /home/ubuntu/.ssh/config
+
+# Command to clone your private repo via SSH using the Private key
+sudo -u ubuntu git clone git@github.com:fu-ceverhart/ITMO463.git /home/ubuntu/ITMO463
+
+# Copy files into place and enable the systemd timer
+cd /home/ubuntu/ITMO463/module5/M5-assessment-templates-and-code
+
+echo "Copying ./app.py to /usr/local/bin/..."
 sudo cp ./app.py /usr/local/bin/
 echo "Copying ./checkqueue.timer to /etc/systemd/system/..."
 sudo cp ./checkqueue.timer /etc/systemd/system/
